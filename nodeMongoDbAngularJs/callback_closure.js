6caf9c95cf18a2d6549bbab78c7649ca102c3109
MusaZhou <musazhou@gmail.com>
2017-01-04 22:16:24 +0800

    update

---
+++ nodeMongoDbAngularJs/callback_closure.js
@@ -0,0 +1,24 @@
+function logCar(logMsg, callback){
+    process.nextTick(function(){
+        //callback(logMsg);
+        callback();
+    });
+}
+
+var cars = ['Ferrari', 'Porsche', 'Bugatti'];
+
+for(var idx in cars){
+    var message = "Saw a " + cars[idx];
+    logCar(message, function(){
+       console.log('Normal callback: ' + message); 
+    });
+}
+
+for(var idx in cars){
+    var message = 'Saw a ' + cars[idx];
+    (function(msg){
+        logCar(msg, function(){
+            console.log('Closure Callback: ' + msg);
+        });
+    })(message);
+}
